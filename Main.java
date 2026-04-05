d3a33317d11600886e085da723e89f0ca3c44eb7
impzay <ipadgett@mail.umw.edu>
2026-04-04 20:42:44 -0400

    added scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        Scanner scan = new Scanner(System.in); 
+        
+        System.out.println("What is your Isianame?");
+        String inp = scan.nextLine();
+
+        System.out.println("Your name is: " + inp);
     }
 }
