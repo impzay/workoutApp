abf8644c3109e4e2e6c5b73f550f36e930e20d78
impzay <ipadgett@mail.umw.edu>
2026-04-04 20:50:03 -0400

    more tests...

--- Main.java
+++ Main.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in); 
-        
-        System.out.println("What is your name?");
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("rizzler");
+        System.out.println("I like iced mochas.");
+
     }
 }
