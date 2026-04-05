8442063374ea891eb9f2efaa1b78a976b0a8054f
Yosays <yosyasmarta@gmail.com>
2026-04-04 20:45:19 -0400

    Fixed you typo

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in); 
         
-        System.out.println("What is your Isianame?");
+        System.out.println("What is your name?");
         String inp = scan.nextLine();
 
         System.out.println("Your name is: " + inp);
