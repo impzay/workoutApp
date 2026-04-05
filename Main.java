e4b64022378c51e725f8d9e6d1c301802a2d7cc2
impzay <ipadgett@mail.umw.edu>
2026-04-04 20:37:22 -0400

    test

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Gay.");
-        System.out.println("Zay is Gay.");
+        System.out.println("Hello, world!");
     }
 }
