38622751fcb697f1b0f542fc5a019bf640c55be1
Yosays <yosyasmarta@gmail.com>
2026-05-01 20:16:20 -0400

    I wanted to format the code more like an automated app from the get go. I think separating user credentials and physical traits can help with searching later between the two. Also want to switch to hashmaps for storing both credentials and physical traits, it would also help later on when inserting,deleting, and accessing user info in the later stages.

--- UserTraits.java
+++ UserTraits.java
@@ -1,9 +1,9 @@
-public class Stats {
+public class UserTraits {
     int totalCalories;
     int totalWorkouts;
     double avgDuration;
 
-    public Stats(int totalCalories, int totalWorkouts, double avgDuration){
+    public UserTraits(int totalCalories, int totalWorkouts, double avgDuration){
         this.totalCalories = totalCalories;
         this.totalWorkouts = totalWorkouts;
         this.avgDuration = avgDuration;
